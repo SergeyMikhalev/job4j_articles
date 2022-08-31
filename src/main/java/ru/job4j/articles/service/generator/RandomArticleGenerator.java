f953a2820739a6514cbd4ab95cf12777b335d82c
Сергей <tolltroll@yandex.ru>
2022-08-31 19:57:53 +0300

    Initial commit

--- src/main/java/ru/job4j/articles/service/generator/RandomArticleGenerator.java
+++ src/main/java/ru/job4j/articles/service/generator/RandomArticleGenerator.java
@@ -11,11 +11,20 @@ import java.util.stream.Collectors;
 public class RandomArticleGenerator implements ArticleGenerator {
     @Override
     public Article generate(List<Word> words) {
-        var wordsCopy = new ArrayList<>(words);
-        Collections.shuffle(wordsCopy);
+        //var wordsCopy = new ArrayList<>(words);
+        Collections.shuffle(words);
+        /*
         var content = wordsCopy.stream()
                 .map(Word::getValue)
                 .collect(Collectors.joining(" "));
-        return new Article(content);
+         */
+
+        StringBuilder sb = new StringBuilder();
+        words.stream().map(Word::getValue).forEach((String s) -> {
+            sb.append(" ");
+            sb.append(s);
+        });
+        return new Article(sb.deleteCharAt(0).toString());
+        //return new Article(content);
     }
 }
